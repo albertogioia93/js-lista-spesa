138c7104cdbd40b2ae489d9fa2ac20caebf8096f
albertogioia93 <albertogioia@hotmail.it>
2023-02-28 14:53:15 +0100

    lista spesa creata col ciclo while

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Stilare una lista della spesa ma usando il ciclo while al posto del ciclo for
+
+COME SVOLGO QUESTO ESERCIZIO:
+
+
+- creo un elemento ul dentro index.html
+- creo una variabile alla quale assegno un array con dentro una lista della spesa su main.js
+- assegno una variabile all'ul
+- uso il ciclo while per creare degli elementi in index.html
--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-alert('ciao');
+const listaSpesa = ['acqua', 'pane', 'pasta', 'carote', 'uova'];
+
+const ul = document.getElementById('listaspesa');
+
+// modalità ciclo for
+// for(let i = 0; i < listaSpesa.length; i++){
+//     const listItem = document.createElement('li');
+//     listItem.append(listaSpesa[i]);
+//     ul.append(listItem);
+// }
+
+// modalità ciclo while
+
+let i = 0;
+
+while(i < listaSpesa.length){
+    const listItem = document.createElement('li');
+    listItem.append(listaSpesa[i]);
+    ul.append(listItem);
+
+    i++
+}
